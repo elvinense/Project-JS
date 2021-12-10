d1cfdda8b06e32742008ad9aa7be30851aada4ea
Brian Bric <elvinense23@gmail.com>
2021-12-10 01:48:00 -0300

    se modificaron funciones de menu.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Variables globales
 
 let producto1 = "Tortas";
@@ -16,24 +15,6 @@ let stock3 = 60;
 let precioFinal = 0
 
 
-// Funcionas Globales
-
-function calculoPrecio(precio, cantidad) {
-    let precioFinal = precio * cantidad
-    return precioFinal
-}
-
-function promo() {
-    let descuento = parseInt(prompt(`Tiene codigo de 20% de descuento?\n
-                            1- Si 
-                            2- No`))
-    if (descuento == 1)
-        precioFinal = precioFinal * 0.8
-    else if (descuento == 0) return
-    else promo()
-
-}
-
 // Funcion menu
 
 function menu() {
@@ -42,90 +23,90 @@ function menu() {
                             1- Tortas 
                             2- Tartas 
                             3- Cupcakes
-                            4- Finalizar compra y pagar
-                            5- Salir`));
+                            4- Salir`));
 
-    if (compra == 1) compraTorta()
-    else if (compra == 2) compraTarta()
-    else if (compra == 3) compraCupcakes()
-    else if (compra == 4) finalizarCompra()
-    else if (compra == 5) return
+    if (compra == 1 || compra == 2 || compra == 3) compras(compra)
+    else if (compra == 4) return
     else menu()
+
 }
 
 // Ejecucion de compra
 
-function compraTorta() {
+function compras(compra) {
 
-    cantidad = parseInt(prompt("¿Cuantas Tortas Quiere Comprar?"))
-    if (cantidad <= stock1) {
-        stock1 -= cantidad;
-        precioFinal += calculoPrecio(precio1, cantidad)
-        alert("Se Agregaron " + cantidad + " " + producto1 + " a tu carro, quedan " + stock1 + " disponibles")
-        return menu()
+    cantidad = parseInt(prompt("¿Cuantas unidades quiere Comprar?"));
+
+    if (compra == 1) {
+        cambioStock(cantidad, stock1, producto1, precio1)
     }
-    else {
-        alert('No tenemos esa cantidad, solo nos quedan ' + stock1 + " unidades disponibles");
 
-        return menu()
+    if (compra == 2) {
+        cambioStock(cantidad, stock2, producto2, precio2)
     }
-}
 
-function compraTarta() {
-
-    cantidad = parseInt(prompt("¿Cuantas Tartas Quiere Comprar?"))
-    if (cantidad <= stock2) {
-        stock2 -= cantidad;
-        precioFinal += calculoPrecio(precio2, cantidad)
-        alert("Se Agregaron " + cantidad + " " + producto2 + " a tu carro, quedan " + stock2 + " disponibles")
-        return menu()
-    }
-    else {
-        alert('No tenemos esa cantidad, solo nos quedan ' + stock2 + " unidades disponibles");
-
-        return menu()
+    if (compra == 3) {
+        cambioStock(cantidad, stock3, producto3, precio3)
     }
 }
 
-function compraCupcakes() {
+// Cambio Stock
 
-    cantidad = parseInt(prompt("¿Cuantos Cupcakes Quiere Comprar?"))
-    if (cantidad <= stock3) {
-        stock3 -= cantidad;
-        precioFinal += calculoPrecio(precio3, cantidad)
-        alert("Se Agregaron " + cantidad + " " + producto3 + " a tu carro, quedan " + stock3 + " disponibles")
-        return menu()
-    }
+function cambioStock(cantidad, stock, producto, precio) {
+    if (cantidad <= stock) {
+        stock -= cantidad
+
+        alert("Se Agregaron " + cantidad + " " + producto + " a tu carro, quedan " + stock + " disponibles")
+        calculoPrecio(cantidad, precio)
+    } 
+    
     else {
-        alert('No tenemos esa cantidad, solo nos quedan ' + stock3 + " unidades disponibles");
-
-        return menu()
+        alert('No tenemos esa cantidad, solo nos quedan ' + stock + " unidades disponibles");
     }
+
+    return menu()
+
 }
 
-// Ejecucion Finalizar compra/Carro
+// Calculo Precio
 
-function finalizarCompra() {
+function calculoPrecio(cantidad, precio) {
+    let precioFinal = precio * cantidad
+    finalizarCompra(precioFinal)
+
+
+}
+
+// Finalizar Compra
+
+function finalizarCompra(precioFinal) {
 
     if (precioFinal == 0) {
         alert("Tu carro esta vacio")
         return menu()
-    } else {
+    } 
+    
+    else {
         alert("\n Total a pagar: $" + precioFinal)
-        promo()
-        alert("\n 20% de descuento aplicado: Total a pagar: $" + precioFinal)
-        pago()
+        pago(precioFinal)
     }
 }
 
-function pago(){
-    let pagar = prompt ("Con cuanto abonara el total de " + precioFinal);
-    let vuelto = pagar - precioFinal
-    alert("\n Este es su vuelto: $" + vuelto )
-    alert("¡Gracias por su Compra!")
+// Ejecucion Pago
+
+function pago(precioFinal) {
+    let pagar = prompt("Con cuanto abonara el total de " + precioFinal);
+    if (pagar < precioFinal) {
+        alert("El monto ingresado no es suficiente")
+        return pago(precioFinal)
+    } 
     
+    else {
+
+        let vuelto = pagar - precioFinal
+        alert("\n Este es su vuelto: $" + vuelto)
+        alert("¡Gracias por su Compra!")
+    }
 }
 
 menu();
-
-console.log(precioFinal)
